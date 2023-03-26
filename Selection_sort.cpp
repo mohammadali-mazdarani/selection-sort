0cfbb591af5a2c9c1d86805d8abbe0ed8f105b78
Mohammad ali mazdarani <74422713+mohammadali-mazdarani@users.noreply.github.com>
2023-03-25 23:05:55 -0700

    Define length of array and dynamic array

---
+++ Selection_sort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system(
+) or input loop */
+/*branch name
+*
+*
+*
+*/
+//prototype
+//--------------------------
+//micro
+//---------------------
+//global variable
+//-------------------
+int main(int argc, char** argv)
+{
+
+//Define array length
+int len = 10;
+
+//Define pointer array
+int *a;
+
+//Define dynamic array
+a = new int[len];
+
+
+
+
+
+
+
+getch();
+}
+//function variable
+//--------------------------
+//functions
