39eac165ff48626cd2265508bfce08c9e7f854cc
Mohammad ali mazdarani <74422713+mohammadali-mazdarani@users.noreply.github.com>
2023-03-25 23:10:28 -0700

    get valu's random number 0 ~30

--- Selection_sort.cpp
+++ Selection_sort.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <conio.h>
 #include <math.h>
-using namespace std;
+#include <stdlib.h> //for srand() & rand function
+#include <ctime>
+#include <iomanip>
 /* run this program using the console pauser or add your own getch, system(
 ) or input loop */
 /*branch name
@@ -26,6 +28,17 @@ int *a;
 
 //Define dynamic array
 a = new int[len];
+
+//This command for generate number with random
+srand((unsigned)time(NULL));
+
+//Get array value with randome value
+for (int i=0; i<len; ++i)
+{
+
+a[i] = rand()%30; //30 for get number in poriod of 0 ~ 30
+
+}
 
 
 
